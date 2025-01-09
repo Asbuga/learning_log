c0d1eabbf983b17e8511aa5f70e57ab28988d305
andrii buga <bugandreij@gmail.com>
2025-01-09 14:27:37 +0200

    Added simple pages for viewing content.
    1. Created base.html for building pages.
    2. Updated index.html for works with base template.
    2. Created templates for:
    	- Lists all user topics.
    	- List the Entry by Topic.
    3. Created urlpatterns by "All Topics" and by "Selected Topic".
    4. Created views for Topics and Topic.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,7 +7,14 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
+    # Home page.
     path('', views.index, name='index'),
     path('home/', views.index, name='index'),
     path('index/', views.index, name='index'),
+
+    # Page with all themes.
+    path("topics/", views.topics, name='topics'),
+
+    # Topic content page.
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 
 def index(request):
     """The home page of the Learning Log app."""
-    return render(request, "learning_logs/index.html")
+    return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Lists all user topics."""
+    topics = Topic.objects.order_by('date_added')
+    content = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', content)
+
+
+def topic(request, topic_id):
+    """List the Entry by Topic."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
