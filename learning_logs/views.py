1948fee307f13f7a169ece3516329aedcfc84428
andrii buga <bugandreij@gmail.com>
2025-01-13 15:18:58 +0200

    Added restrictions to user information
    1. Added "login_required" to view.py to protect user information.
    2. Added method to verify content owner.
    3. Updated Topic model.
    4. Updated settings.py.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -9,21 +11,29 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """Lists all user topics."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     content = {'topics': topics}
     return render(request, 'learning_logs/topics.html', content)
 
 
+def check_topic_owner(request, topic):
+    if topic.owner != request.user:
+        raise Http404
+
+@login_required
 def topic(request, topic_id):
     """List the Entry by Topic."""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """Defines a new topic."""
     if request.method != 'POST':
@@ -31,7 +41,9 @@ def new_topic(request):
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
         
     # Returns empty form.
@@ -39,9 +51,12 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Adds a new entry in topics."""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
+
     if request.method != 'POST':
         form = EntryForm()
     else:
@@ -56,10 +71,12 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Edits exist entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
